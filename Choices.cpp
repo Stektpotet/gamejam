401d3302b2e702f17e647af5093f9aae20977542
Halvor Bakken Smedås <stektpotet@gmail.com>
2016-09-11 14:38:48 +0200

    Updated to a vector stucture for choices
    Now choices should be more flexible as you can add and remove as you
    please. This also makes it easy to display only the active choices, as
    they are the only ones that should exist in the all()-return

--- Choices.cpp
+++ Choices.cpp
@@ -1,17 +1,46 @@
 #include "Choices.h"
 
+void Choices::add(string desc, function<void(int)> choiceCallback)
+{
+	allChoices.push_back({desc, choiceCallback});
+}
+
+Choice Choices::pop(int index)
+{
+	Choice poppedChoice = allChoices[index];
+	allChoices.erase(allChoices.begin() + index);
+	return poppedChoice;
+}
+//TODO manage input so we dont try to access unavailable indices
 Choice Choices::getChoice(int index)
 {
+	if (size() < index)
+	{
+		//Invalid input
+		cout << index << ": No viable action";
+		index = 0; //This needs to be handled somehow, the player should only be able to use the speccefied actions(indices).
+	}
 	return allChoices[index];
 }
 
 void Choices::setChoice(int index, string desc, function<void(int)> choiceCallback)
 {
+	if (size() < index)
+	{
+		//Invalid input
+		cout << index << ": No viable action";
+		index = 0; //This needs to be handled somehow, the player should only be able to use the speccefied actions(indices).
+	}
 	allChoices[index].decsription = desc;
 	allChoices[index].callback = choiceCallback;
 }
 
+int Choices::size()
+{
+	return (int)allChoices.size();
+}
+
 Choice* Choices::all()
 {
-	return allChoices;
+	return &allChoices[0];
 }
